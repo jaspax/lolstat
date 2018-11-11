508cd15b33cfded28b6365b405732fdd9bdf36f2
JS Bangs <jaspax@gmail.com>
2018-11-11 23:10:30 +0200

    add router suitable for heroku app

--- index.js
+++ index.js
@@ -28,3 +28,12 @@ app.get('/api/stat', async (req, res) => {
         return res.status(500).send(ex.message);
     }
 });
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
